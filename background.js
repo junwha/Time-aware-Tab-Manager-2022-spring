82c58c526db2baeed2bb485316d58bd6c4648e1f
준화 <junwha0511@naver.com>
2022-05-13 20:29:06 +0900

    Implement tab grouping

--- background.js
+++ background.js
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 'use strict';
 
-const ALARM_INTERVAL = 5*1000;
+const ALARM_INTERVAL = 5 * 1000;
 const THRESHOLD = [20, 40];
 
 function getUnixTime() {
@@ -71,20 +71,20 @@ function getTabFromList(tabid, windowid) {
 
 
 chrome.alarms.onAlarm.addListener(() => {
-  chrome.action.setBadgeText({ text: '' });
-  chrome.notifications.create({
-    type: 'basic',
-    //iconUrl: 'stay_hydrated.png',
-    title: 'You have an unused tab',
-    message: 'Check tab status',
-    priority: 0
-  });
+    chrome.action.setBadgeText({ text: '' });
+    chrome.notifications.create({
+        type: 'basic',
+        //iconUrl: 'stay_hydrated.png',
+        title: 'You have an unused tab',
+        message: 'Check tab status',
+        priority: 0
+    });
 });
 
 chrome.runtime.onStartup.addListener(
-  async () => {
-    chrome.runtime.connect();
-  }
+    async () => {
+        chrome.runtime.connect();
+    }
 );
 
 chrome.tabs.onActivated.addListener(
@@ -98,27 +98,99 @@ chrome.tabs.onActivated.addListener(
 
 //add tab into 
 chrome.tabs.onCreated.addListener(
-  async (tab) => {
-    console.log(`tab created ${tab.id} ${tab.windowId}`);
-    var current_date = new Date();
-    console.log('created time');
-    console.log(current_date);
-    var tabInfo = new TabInfo(tab.id, tab.windowId);
-    tabInfoList.push(tabInfo);
-  }
+    async (tab) => {
+        console.log(`tab created ${tab.id} ${tab.windowId}`);
+        var current_date = new Date();
+        console.log('created time');
+        console.log(current_date);
+        var tabInfo = new TabInfo(tab.id, tab.windowId);
+        tabInfoList.push(tabInfo);
+    }
 );
 
 //delete tab from list
 chrome.tabs.onRemoved.addListener(
-  async (tabid, info) => {
-    console.log(`tab deleted ${tabid} ${info.windowId}`);
-    var current_date = new Date();
-    console.log('deleted time');
-    console.log(current_date);
-    tabInfoList = removeTabFromList(tabid, info.windowId);
-  }
+    async (tabid, info) => {
+        console.log(`tab deleted ${tabid} ${info.windowId}`);
+        var current_date = new Date();
+        console.log('deleted time');
+        console.log(current_date);
+        tabInfoList = removeTabFromList(tabid, info.windowId);
+    }
 );
 
 setInterval(() => {
     let [firstStage, secondStage] = getTabGroupByTime();
+
+    groupTabs(firstStage, THRESHOLD[0]);
+    groupTabs(secondStage, THRESHOLD[1]);
+
 }, ALARM_INTERVAL);
+
+
+function groupAdjacentTIDs(tab_list) {
+    if (tab_list.length == 0) return [];
+
+    var all_list = new Array();
+
+    tab_list.sort(function (a, b) {
+        return a.index - b.index;
+    });
+
+    var last_index = tab_list[0].index - 1;
+    var each_list = new Array();
+
+    for (const tab of tab_list) {
+        if (tab.index - 1 != last_index) {
+            all_list.push(each_list);
+            each_list = new Array();
+        }
+
+        each_list.push(tab.id);
+        last_index = tab.index;
+    }
+
+    if (each_list.length != 0) all_list.push(each_list);
+
+    return all_list;
+}
+
+
+async function groupTabs(tab_info_list, elapsed_time) {
+    var tab_list = [];
+
+    for (const tab_info of tab_info_list) {
+        tab_list.push(await chrome.tabs.get(tab_info.getTabId()));
+    }
+
+    var all_list = groupAdjacentTIDs(tab_list);
+
+    if (all_list.length == 0) return;
+
+    for (const tid_list of all_list) {
+        try {
+            chrome.tabs.group(
+                { tabIds: tid_list },
+                (gid) => {
+                    var _color, _time_info;
+
+                    if (elapsed_time >= 40) {
+                        _time_info = "40s ago";
+                        _color = "red";
+                    } else if (elapsed_time >= 20) {
+                        _time_info = "20s ago";
+                        _color = "yellow";
+                    } else {
+                        return;
+                    }
+
+                    chrome.tabGroups.update(gid, {
+                        color: _color,
+                        title: _time_info
+                    });
+                });
+        } catch {
+            console.log("We lost the tap");
+        }
+    }
+}
