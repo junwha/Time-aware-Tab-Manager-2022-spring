f378ec514d74101e9e96b57dc7eefb8e3be50547
준화 <junwha0511@naver.com>
2022-06-07 02:42:49 +0900

    disable debug

--- background.js
+++ background.js
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 'use strict';
 
-const DEBUG = true;
+const DEBUG = false;
 const ALARM_INTERVAL = 1; // Threshold for update groups (minute)
 const SKIP_THRESHOLD = 2000; // Threshold for removing current visiting tab from target (milliseconds)
 const MAX_TRIAL = 20;
@@ -655,6 +655,7 @@ async function group(tidList, elapsedTime, windowId, trial) {
                     var THRESHOLD = global.getTHRESHOLD();
                     if (gid === undefined)
                         return;
+
                     // console.log(gid);
                     var _color, _timeInfo;
 
